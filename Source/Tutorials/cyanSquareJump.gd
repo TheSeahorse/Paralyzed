e51febd845fc648c47c692c35c357f268687aced
Alexander Lahti <alexander.lahti.2359@student.uu.se>
2020-08-27 19:25:08 +0200

    tutorial popups done

--- Source/Tutorials/cyanSquareJump.gd
+++ Source/Tutorials/cyanSquareJump.gd
@@ -1,5 +1,10 @@
 extends CanvasLayer
 
+
+func _ready() -> void:
+	$Sprite/Label.set_text("to make an enemy \nsquare jump, press \n" + InputMap.get_action_list("action")[0].as_text() + " while being \nthe same          as it")
+
+
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_released("action"):
 		get_tree().paused = false
--- Source/Tutorials/introText.gd
+++ Source/Tutorials/introText.gd
@@ -1,7 +1,13 @@
 extends CanvasLayer
 
 func _ready() -> void:
-	$Sprite/continue_label.set_text("press " + InputMap.get_action_list("action")[0].as_text() + " to continue")
+	var intro_node = get_node_or_null("Sprite/intro_label")
+	if intro_node != null:
+		intro_node.set_text("press " + InputMap.get_action_list("action")[0].as_text() + " to continue")
+	var recap_node = get_node_or_null("Sprite/recap_label")
+	if recap_node != null:
+		recap_node.set_text("Great!\n" + InputMap.get_action_list("cyan")[0].as_text() + " " + InputMap.get_action_list("red")[0].as_text() + " " + InputMap.get_action_list("purple")[0].as_text() + " " + InputMap.get_action_list("yellow")[0].as_text() + " and " + InputMap.get_action_list("action")[0].as_text() + "\nare the only buttons\nyou'll need to beat\nthis game. good luck!")
+
 
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_released("action"):
