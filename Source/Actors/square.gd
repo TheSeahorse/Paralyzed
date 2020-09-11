0f8f67b80e72e601383204388abb5f41549eaee2
Alexander Lahti <alexander.lahti.2359@student.uu.se>
2020-09-11 17:11:51 +0200

    messing around with particles, it's hard, didnt get anywhere

--- Source/Actors/square.gd
+++ Source/Actors/square.gd
@@ -6,12 +6,13 @@ var PLAYER_OVER: = false #true if the player is over
 var ENABLED: = false #turns true the first time physics process is run
 var GRACE_FRAMES: = 0 #how many frames the player can miss the jump on a square (if its in the air) but the square still jumps
 var TOGGLE_ACTION: = false #if "action" has been input by the user
-var ON_FLOOR: = false #if the square is on any kind of KinematicBody2D
+var ON_FLOOR: = true #if the square is on any kind of KinematicBody2D
 var SPRING_JUMP: = 0 #variable to control the flingy jump of the square, if greater than 0 we're calculating the jump
 
 func _ready() -> void:
 	$square_sprite.play(COLOR)
 	set_physics_process(false)
+
 
 func _physics_process(delta: float) -> void:
 	if !ENABLED:
@@ -24,11 +25,40 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_tileMap_body_entered(_body: Node) -> void: #Area2D, not tilemap
-	ON_FLOOR = true
+	if not ON_FLOOR:
+		ON_FLOOR = true
+		set_particle_color()
+		$Dust1.set_visible(true)
+		$Dust2.set_visible(true)
+		$Dust1.set_emitting(true)
+		$Dust2.set_emitting(true)
+
 
 
 func _on_tileMap_body_exited(_body: Node) -> void: #Area2D, not tilemap
 	ON_FLOOR = false #this gets changed faster in the jump functions
+	print("hey")
+
+
+
+func set_particle_color():
+	var material1 = $Dust1.get_process_material()
+	var material2 = $Dust2.get_process_material()
+	match COLOR:
+		"cyan":
+			material1.set_color(Color(99, 172, 190, 255))
+			material2.set_color(Color(99, 172, 190, 255))
+		"red":
+			material1.set_color(Color(238, 68, 47, 255))
+			material2.set_color(Color(238, 68, 47, 255))
+		"purple":
+			material1.set_color(Color(96, 26, 74, 255))
+			material2.set_color(Color(96, 26, 74, 255))
+		"yellow":
+			material1.set_color(Color(255, 232, 41, 255))
+			material2.set_color(Color(255, 232, 41, 255))
+	$Dust1.set_process_material(material1)
+	$Dust2.set_process_material(material2)
 
 
 func action():
