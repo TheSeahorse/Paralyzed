59b8518d56a20856d9d263fce22389b68bf2ebd8
TheSeahorse <alexanderlahti@hotmail.com>
2020-10-16 21:57:07 +0200

    changed tutorial, now fluid, no pauses

--- Source/Levels/globalLevel.gd
+++ Source/Levels/globalLevel.gd
@@ -1,12 +1,26 @@
 extends Node
 class_name GlobalLevel
 
+
+func _ready():
+	if $jump_input:
+		$jump_input.set_text(InputMap.get_action_list("action")[0].as_text())
+		$jump2_input.set_text(InputMap.get_action_list("action")[0].as_text())
+		$become_red.set_text("press " + InputMap.get_action_list("red")[0].as_text() + " to\nbecome red")
+		$cyan.set_text(InputMap.get_action_list("cyan")[0].as_text())
+		$red.set_text(InputMap.get_action_list("red")[0].as_text())
+		$purple.set_text(InputMap.get_action_list("purple")[0].as_text())
+		$yellow.set_text(InputMap.get_action_list("yellow")[0].as_text())
+		$pause_input.set_text(InputMap.get_action_list("escape")[0].as_text())
+
+
 func action(color: String):
 	for child in get_children():
 		if child is Square and color == child.COLOR:
 			child.action()
 		if child is Car and color == child.COLOR:
 			child.TOGGLE_ACTION = true
+
 
 func play_animation(color: String):
 	for child in get_children():
